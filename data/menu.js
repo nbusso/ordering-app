db6dd32837fefd1c74bbc6d69f8ebb1078a2c508
Nicolás Busso <nicolasbusso@gmail.com>
2025-06-25 10:02:38 -0300

    ffinished structure

---
+++ data/menu.js
@@ -0,0 +1,17 @@
+export const menuList = [
+    {
+        name: 'Pizza',
+        ingredients: [ 'pepperoni', 'mushroom', 'mozarella' ],
+        price: 14
+    },
+    {
+        name: 'Hamburguer',
+        ingredients: [ 'beef', 'cheese', 'lettuce' ],
+        price: 12
+    },
+    {
+        name: 'Beer',
+        ingredients: [ 'grain', 'hops', 'yeast', 'water' ],
+        price: 12
+    },
+]
